434ea652483a37902ffee157ea80a27bf4c9b7c4
Marco L. Buschini <marco.buschini@gmail.com>
2015-08-21 16:11:59 +0200

    Properly loaded the entity manager.

--- Tests/Doctrine/DagConnectFunctionalTest.php
+++ Tests/Doctrine/DagConnectFunctionalTest.php
@@ -11,7 +11,7 @@ class DagConnectFunctionalTest extends IntegrationTestCase
     /**
      * @var \Doctrine\ORM\EntityManager
      */
-    private $em;
+    private $em = static::getEntityManager();
 
     public function testInitialCreation()
     {
