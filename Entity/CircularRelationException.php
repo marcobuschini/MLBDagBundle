19267489af3891fba7930fa78b4c653aadd674f7
Marco Buschini <marco.buschini@gmail.com>
2015-08-25 15:16:59 +0000

    Completed two missing exceptions.

--- Entity/CircularRelationException.php
+++ Entity/CircularRelationException.php
@@ -2,3 +2,12 @@
 namespace MLB\DagBundle\Tests\Doctrine;
 
 class CircularRelationException extends Exception {
+ 
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        // some code
+    
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }   
+}
--- Entity/EdgeDoesNotExistException.php
+++ Entity/EdgeDoesNotExistException.php
@@ -3,5 +3,11 @@ namespace MLB\DagBundle\Tests\Doctrine;
 
 class EdgeDoesNotExistException extends Exception {
 
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        // some code
     
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
 }
