0b2ba03e9978c7dc03db6d4f5b24cbd441aea240
Marco Buschini <marco.buschini@gmail.com>
2015-08-26 11:41:14 +0000

    Oopsed removing a function.

--- Tests/IntegrationTestCase.php
+++ Tests/IntegrationTestCase.php
@@ -33,14 +33,12 @@ abstract class IntegrationTestCase extends WebTestCase {
 	/**
 	 * {@inheritDoc}
 	 */
-	/*
 	protected static function createKernel(array $options = array()) {
 		$environment = isset($options['environment']) ? $options['environment'] : 'test';
 		$configFile = isset($options['config']) ? $options['config'] : 'config.yml';
 
 		return new AppKernel($environment, $configFile);
 	}
-	*/
 
 	protected static function rebuildDatabase() {
 		$application = new Application(static::$kernel);
